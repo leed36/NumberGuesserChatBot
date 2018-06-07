094e21cf4f7ef1a09adcb0041bd5662d4d35f8d9
Daniel <leed36@DESKTOP-RB3N1RQ.localdomain>
2018-06-07 01:49:24 -0700

    Added extra implementations for fun

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NumberGuesserChatBot
 Chatbot using Nodejs that guesses a number that the user is thinking
+Runs with Bot Framework Emulator from Azure Bot Documentation
+Type in terminal: node app.js
--- app.js
+++ app.js
@@ -8,6 +8,9 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var name;
+var reprompt = false;
+var secondAct = false;
 //holds the memory from the bot
 var inMemoryStorage = new builder.MemoryBotStorage();
 
@@ -28,69 +31,91 @@ var connector = new builder.ChatConnector(
 
 //listens for messages
 server.post('/api/messages', connector.listen());
-var name;
-function divide4(x) {
-   return (x / 4);
-};
+
 //receives and echoes back
 var bot = new builder.UniversalBot(connector, [
    function (session) {
-      session.send("Hello, I think I'm a ChatBot or Chatting Robot. And I will miraculously guess a number.");
-      session.beginDialog('askForName');
+      if (!secondAct) {
+         session.send("Hello, I think I'm ChatBot or Chatting Robot. And I will miraculously guess your number.");
+         session.beginDialog('firstActivity');
+      }
+   },
+   function (session, results) {
+      if (results.response === "no") {
+         session.send("Awww, I guess I messed up. ");
+      } else {
+         session.send("Oh I actually got it. Thank you for playing! I hope you have fun with Ferdinand.");
+      }
+      session.send("*ChatBot leaves* *Ferdinand enters*")
+      session.send("Hello there, My name is Ferdinand and I would like to quiz your math skills.");
+      session.beginDialog('secondActivity');
+   }
+
+]).set('storage', inMemoryStorage);
+
+
+bot.dialog('firstActivity', [
+   function(session) {
+      if (!reprompt) {
+         builder.Prompts.text(session, "What is your name?");
+      } else {
+         reprompt = false;
+         session.send("Let's try this again. Type anything to continue or goodbye to stop.");
+      }
    },
    function (session, results) {
       session.dialogData.name = results.response;
       name = results.response;
-      session.beginDialog('askForNumber');
+      session.send("Hi " + name + "! Please think of a number between 1 - 10 and remember it.");
+      builder.Prompts.confirm(session, "Is your number greater than 5? Yes or No.");
+
    },
    function (session, results) {
-      session.dialogData.greaterThan5 = results.response;
-      session.beginDialog('askForMath');
-   },
-   function (session,results) {
-      session.dialogData.mathNumber = results.response;
-      session.endConversation("Your number is " + divide4(results.response));
-   }
-]).set('storage', inMemoryStorage);
-
-
-
-bot.dialog('askForName', [
-   function(session) {
-      builder.Prompts.text(session, "What is your name?");
+      session.dialogData.grtrFive = results.response;
+      builder.Prompts.number(session, "What is your number multiplied by 4?");
    },
    function (session, results) {
-      session.endDialogWithResult(results);
+      session.dialogData.multFour = results.response;
+      var number = divide4(results.response);
+      if (number % 1 === 0) {
+         secondAct = true;
+         session.send("Your number is " + number);
+         builder.Prompts.confirm(session, "Am I right?");
+      } else {
+         session.send("You multiplied your number incorrectly.");
+         reprompt = true;
+         session.replaceDialog('firstActivity');
+      }
    }
 ])
-.beginDialogAction('nameHelpAction', 'help', { matches: /^help$/i });
+.endConversationAction('endConversationAction', 'Ok, goodbye!', {
+    matches: /^goodbye$/i
+})
+.beginDialogAction('helpAction', 'help', { matches: /^help$/i });
 
 bot.dialog('help', function(session, args, next) {
    var msg = "Sorry, I don't know what to do either. -.-";
    session.endDialog(msg);
 })
 
-//ask user to think of a number
-bot.dialog('askForNumber', [
-   function (session) {
-      session.send("Hi " + name + "! Please think of a number between 1 - 10 and remember it.");
-      builder.Prompts.text(session, "Is your number greater than 5? Yes or No.");
-   },
-   function (session, results) {
-      session.endDialogWithResult(results);
-   }
-])
-.beginDialogAction('nameHelpAction', 'help', { matches: /^help$/i });
-
-bot.dialog('askForMath', [
+bot.dialog('secondActivity', [
    function(session) {
-      builder.Prompts.text(session, "What is your number multiplied by 4?");
-   },
-   function (session, results) {
-      session.endDialogWithResult(results);
+      
    }
 ])
-.beginDialogAction('nameHelpAction', 'help', { matches: /^help$/i });
+.endConversationAction('endConversationAction', 'Ok, goodbye!', {
+    matches: /^goodbye$/i
+})
+.beginDialogAction('helpAction', 'help2', { matches: /^help$/i });
+
+bot.dialog('help2', function(session, args, next) {
+   var msg = "Umm I'll just repeat what I said.";
+   session.endDialog(msg);
+})
+//function for dividing the number given in one of the dialogs
+function divide4(x) {
+   return (x / 4);
+};
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
